303ea58c19830aaa59fb7d7ff7dd3d84a0592d83
祝獻的巴基力 <68413810+Valkierja@users.noreply.github.com>
2023-04-18 00:51:05 +0800

    测试中

--- main.user.js
+++ main.user.js
@@ -13,13 +13,19 @@
 // ==/UserScript==
 
 function autoPj() {
-    document.getElementById('wpj_yq').click()//转换到未评价页面
+    // document.getElementById('wpj_yq').click()//转换到未评价页面
     var index = 1;
     while (true) { // 无限循环
         try {
-            var teacher_id = '#wpjkc > li:nth-child(' + index.toString(10) + ')'
+            //#wpjkc > li:nth-child(1)
+            var teacher_id = "#wpjkc > li:nth-child(" + index.toString(10) + ")"
+            console.log(teacher_id)
             document.querySelector(teacher_id).click() //进入对应教师页面
-            giveScore()
+            // giveScore()
+            for (var i = 0; i < 9; i++) {
+                $("input[name='pjf']")[i].value= "80"
+        
+            }
             ++index
         } catch (e) {
             if (e instanceof SyntaxError) {
@@ -32,23 +38,33 @@ function autoPj() {
     }
 } autoPj();
 
-function giveScore() {
-    var score = '80'; //教师分数, 默认80分
 
-    //以下是分数框哈希ID, 似乎是定值，没经过大范围测试
-    var ID_hash_first_part = '#li_2_F9066C26C99'
-    var ID_hash_last_part = '89D1E05383001CAC1890 > input[type=hidden]:nth-child(2)'
-    var ID_hash_mid_part = 4; // 循环从4开始
+// function giveScore() {
+//     for (var i = 0; i < 9; i++) {
+//         $("input[name='pjf']")[i].value= "80"
 
-    for (; ID_hash_mid_part < 13; ID_hash_mid_part++) {
-        var ID_hash_mid_part_hex = ID_hash_mid_part.toString(16);
-        var Selector1 = ID_hash_first_part + ID_hash_mid_part_hex + ID_hash_last_part
-        //#li_2_F9066C26C99489D1E05383001CAC1890 > input[type=hidden]:nth-child(2)
-        //#li_2_F9066C26C99589D1E05383001CAC1890 > input[type=hidden]:nth-child(2)
-        // var Selector2 = '#' + ID_hash + ' > input[type=hidden]:nth-child(3)'
-        document.querySelector(Selector1).value = score//BUG:分数不是这样写的, 但是到底是怎么写进去分数的没太看懂
-        // document.querySelector(Selector2).value = score
-    }
+//     }
+// }
 
-    document.querySelector('#submit').click()//提交该老师的分数
-}
+
+//旧版笨办法 有bug
+// function giveScore() {
+//     var score = '80'; //教师分数, 默认80分
+
+//     //以下是分数框哈希ID, 似乎是定值，没经过大范围测试
+//     var ID_hash_first_part = '#li_2_F9066C26C99'
+//     var ID_hash_last_part = '89D1E05383001CAC1890 > input[type=hidden]:nth-child(2)'
+//     var ID_hash_mid_part = 4; // 循环从4开始
+
+//     for (; ID_hash_mid_part < 13; ID_hash_mid_part++) {
+//         var ID_hash_mid_part_hex = ID_hash_mid_part.toString(16);
+//         var Selector1 = ID_hash_first_part + ID_hash_mid_part_hex + ID_hash_last_part
+//         //#li_2_F9066C26C99489D1E05383001CAC1890 > input[type=hidden]:nth-child(2)
+//         //#li_2_F9066C26C99589D1E05383001CAC1890 > input[type=hidden]:nth-child(2)
+//         // var Selector2 = '#' + ID_hash + ' > input[type=hidden]:nth-child(3)'
+//         document.querySelector(Selector1).value = score//BUG:分数不是这样写的, 但是到底是怎么写进去分数的没太看懂
+//         // document.querySelector(Selector2).value = score
+//     }
+
+//     document.querySelector('#submit').click()//提交该老师的分数
+// }
