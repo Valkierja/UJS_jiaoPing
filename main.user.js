1f97bdc42fd022cb2e1e2cdbb26e5667798c7682
祝獻的巴基力 <68413810+Valkierja@users.noreply.github.com>
2023-04-18 13:10:39 +0800

    测试

--- main.user.js
+++ main.user.js
@@ -1,4 +1,3 @@
-/* globals jQuery, $ */
 // ==UserScript==
 // @name         Fuck_ujs_Pj
 // @namespace    http://tampermonkey.net/
@@ -11,25 +10,57 @@
 // @updateURL    https://raw.githubusercontent.com/KSroido/UJS_jiaoPing/main/main.user.js
 // @match        https://*.ujs.edu.cn/*kcgcpj_cxKcgcpjxxIndex.html*
 // @match        http://*.ujs.edu.cn/*kcgcpj_cxKcgcpjxxIndex.html*
-// @require      https://cdn.bootcdn.net/ajax/libs/jquery/3.6.4/jquery.js
-// @grant        GM_addStyle
 // ==/UserScript==
 
-function autoPj() {
-    // document.getElementById('wpj_yq').click()//转换到未评价页面
-    var hash = ['#li_2_F9066C26C99489D1E05383001CAC1890', '#li_2_F9066C26C99589D1E05383001CAC1890', '#li_2_F9066C26C99689D1E05383001CAC1890', '#li_2_F9066C26C99789D1E05383001CAC1890', '#li_2_F9066C26C99889D1E05383001CAC1890', '#li_2_F9066C26C99989D1E05383001CAC1890', '#li_2_F9066C26C99a89D1E05383001CAC1890', '#li_2_F9066C26C99b89D1E05383001CAC1890', '#li_2_F9066C26C99c89D1E05383001CAC1890']
+var giveScoreWrapperFLAG = false
 
+function teacher() {
+    console.log("teacher() start")
     //#wpjkc > li:nth-child(1)
     var teacher_id = "#wpjkc > li:nth-child(1)"
-    // console.log(teacher_id)
+
     document.querySelector(teacher_id).click() //进入对应教师页面
-    // giveScore()
-    for (var i = 0; i < 9; i++) {
-        document.querySelector(hash[i] + ' > input[type=hidden]:nth-child(2)').value = 80
+    console.log("teacher() end")
+
+}
+
+function giveScoreWrapper() {
+    if (giveScoreWrapperFLAG && document.querySelector("#li_2_F9066C26C99489D1E05383001CAC1890 > input[type=hidden]:nth-child(2)")) {
+        giveScore()
+        return true
     }
+    giveScoreWrapperFLAG = true
+    return false
+}
 
+function giveScore() {
+    console.log("teacher() start")
+    var hash = ["#li_2_F9066C26C99489D1E05383001CAC1890", "#li_2_F9066C26C99589D1E05383001CAC1890", "#li_2_F9066C26C99689D1E05383001CAC1890", "#li_2_F9066C26C99789D1E05383001CAC1890", "#li_2_F9066C26C99889D1E05383001CAC1890", "#li_2_F9066C26C99989D1E05383001CAC1890", "#li_2_F9066C26C99a89D1E05383001CAC1890", "#li_2_F9066C26C99b89D1E05383001CAC1890", "#li_2_F9066C26C99c89D1E05383001CAC1890"]
+    // while (true) {
+    //     if (document.querySelector(hash[0] + " > input[type=hidden]:nth-child(2)")) {
+    //         for (var i = 0; i < 9; i++) {
+    //             var temp = hash[i] + " > input[type=hidden]:nth-child(2)"
+    //             document.querySelector(temp).value = '80'
+    //         }
+    //     }
+    // }
+    if (giveScoreWrapperFLAG) {
+        for (var i = 0; i < 9; i++) {
+            var temp = hash[i] + " > input[type=hidden]:nth-child(2)"
+            document.querySelector(temp).value = '80'
+        }
+        console.log("teacher() end")
+        document.querySelector("#submit").click()
+        return
+    }
+    giveScoreWrapperFLAG = true
+    setTimeout(giveScore(), 5000);
+    return
+}
+function autoPj() {
+    //setTimeout(teacher(), 5000);
+    teacher()
 
-    document.querySelector('#submit').click()
+    window.addEventListener('load', ()=>{console.log('load ready'); giveScore()})
     //location.reload();
 } autoPj();
-
