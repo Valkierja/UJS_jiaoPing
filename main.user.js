f0f5eaf08d349cfd8b10a162a714c38bd3274b15
祝獻的巴基力 <68413810+Valkierja@users.noreply.github.com>
2023-04-18 13:36:36 +0800

    1

--- main.user.js
+++ main.user.js
@@ -12,7 +12,7 @@
 // @match        http://*.ujs.edu.cn/*kcgcpj_cxKcgcpjxxIndex.html*
 // ==/UserScript==
 
-var giveScoreWrapperFLAG = false
+
 
 function teacher() {
     console.log("teacher() start")
@@ -24,19 +24,22 @@ function teacher() {
 
 }
 
-function giveScoreWrapper() {
-    if (giveScoreWrapperFLAG && document.querySelector("#li_2_F9066C26C99489D1E05383001CAC1890 > input[type=hidden]:nth-child(2)")) {
-        giveScore()
-        return true
-    }
-    giveScoreWrapperFLAG = true
-    return false
-}
+
 
 function giveScore() {
     console.log("teacher() start")
     var hash = ["#li_2_F9066C26C99489D1E05383001CAC1890", "#li_2_F9066C26C99589D1E05383001CAC1890", "#li_2_F9066C26C99689D1E05383001CAC1890", "#li_2_F9066C26C99789D1E05383001CAC1890", "#li_2_F9066C26C99889D1E05383001CAC1890", "#li_2_F9066C26C99989D1E05383001CAC1890", "#li_2_F9066C26C99a89D1E05383001CAC1890", "#li_2_F9066C26C99b89D1E05383001CAC1890", "#li_2_F9066C26C99c89D1E05383001CAC1890"]
-    // while (true) {
+ 
+    document.querySelector("#li_2_F9066C26C99489D1E05383001CAC1890 > input[type=hidden]:nth-child(2)").value = '80' //不能用for循环, 会有堵塞的问题
+    document.querySelector("#li_2_F9066C26C99589D1E05383001CAC1890 > input[type=hidden]:nth-child(2)").value = '80'
+    document.querySelector("#li_2_F9066C26C99689D1E05383001CAC1890 > input[type=hidden]:nth-child(2)").value = '80'
+    document.querySelector("#li_2_F9066C26C99789D1E05383001CAC1890 > input[type=hidden]:nth-child(2)").value = '80'
+    document.querySelector("#li_2_F9066C26C99889D1E05383001CAC1890 > input[type=hidden]:nth-child(2)").value = '80'
+    document.querySelector("#li_2_F9066C26C99989D1E05383001CAC1890 > input[type=hidden]:nth-child(2)").value = '80'
+    document.querySelector("#li_2_F9066C26C99A89D1E05383001CAC1890 > input[type=hidden]:nth-child(2)").value = '80'
+    document.querySelector("#li_2_F9066C26C99B89D1E05383001CAC1890 > input[type=hidden]:nth-child(2)").value = '80'
+    document.querySelector("#li_2_F9066C26C99C89D1E05383001CAC1890 > input[type=hidden]:nth-child(2)").value = '80'
+   // while (true) {
     //     if (document.querySelector(hash[0] + " > input[type=hidden]:nth-child(2)")) {
     //         for (var i = 0; i < 9; i++) {
     //             var temp = hash[i] + " > input[type=hidden]:nth-child(2)"
@@ -44,23 +47,28 @@ function giveScore() {
     //         }
     //     }
     // }
-    if (giveScoreWrapperFLAG) {
-        for (var i = 0; i < 9; i++) {
-            var temp = hash[i] + " > input[type=hidden]:nth-child(2)"
-            document.querySelector(temp).value = '80'
-        }
-        console.log("teacher() end")
-        document.querySelector("#submit").click()
-        return
-    }
-    giveScoreWrapperFLAG = true
-    setTimeout(giveScore(), 5000);
-    return
 }
 function autoPj() {
     //setTimeout(teacher(), 5000);
     teacher()
 
-    window.addEventListener('load', ()=>{console.log('load ready'); giveScore()})
+    window.addEventListener('load', () => { console.log('load ready'); giveScore() })
+    giveScore()
     //location.reload();
 } autoPj();
+
+
+// console.log("teacher() start")
+// //#wpjkc > li:nth-child(1)
+// var teacher_id = "#wpjkc > li:nth-child(1)"
+
+// document.querySelector(teacher_id).click() //进入对应教师页面
+// console.log("teacher() end")
+// for (var jindex = 0; jindex < 100000; jindex++) {
+//     console.log("settime")
+// }
+
+// for (var iindex = 0; iindex < 9; iindex++) {
+//     var temp = hash[iindex] + " > input[type=hidden]:nth-child(2)"
+//     document.querySelector(temp).value = '80'
+// }
