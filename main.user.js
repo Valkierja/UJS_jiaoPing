dbe9483dbd0fdaa1586a2c29a0bcce3ba88ac007
祝獻的巴基力 <68413810+Valkierja@users.noreply.github.com>
2023-04-18 11:55:03 +0800

    测试

--- main.user.js
+++ main.user.js
@@ -17,7 +17,7 @@
 
 function autoPj() {
     // document.getElementById('wpj_yq').click()//转换到未评价页面
-
+    var hash = ['#li_2_F9066C26C99489D1E05383001CAC1890', '#li_2_F9066C26C99589D1E05383001CAC1890', '#li_2_F9066C26C99689D1E05383001CAC1890', '#li_2_F9066C26C99789D1E05383001CAC1890', '#li_2_F9066C26C99889D1E05383001CAC1890', '#li_2_F9066C26C99989D1E05383001CAC1890', '#li_2_F9066C26C99a89D1E05383001CAC1890', '#li_2_F9066C26C99b89D1E05383001CAC1890', '#li_2_F9066C26C99c89D1E05383001CAC1890']
 
     //#wpjkc > li:nth-child(1)
     var teacher_id = "#wpjkc > li:nth-child(1)"
@@ -25,9 +25,11 @@ function autoPj() {
     document.querySelector(teacher_id).click() //进入对应教师页面
     // giveScore()
     for (var i = 0; i < 9; i++) {
-        $("input[name='pjf']")[i].value = "80"
+        document.querySelector(hash[i] + ' > input[type=hidden]:nth-child(2)').value = 80
     }
+
+
     document.querySelector('#submit').click()
     //location.reload();
-}autoPj();
+} autoPj();
 
